64cbccb0249978c524b8141f732e43c9f3a70108
f-sky <939949352@qq.com>
2021-09-04 11:23:00 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -7,5 +7,5 @@ git rev-parse --short HEAD >> out/published-commit-ids.html
 chmod -R o+r out
 
 # Edit these two lines to match your own server and username.
-rsync -aP out/ jiamingsun@do.jiamingsun.ml:homepage/
-ssh jiamingsun@do.jiamingsun.ml "chown -R jiamingsun:www-data ~/homepage"
+#rsync -aP out/ jiamingsun@do.jiamingsun.ml:homepage/
+#ssh jiamingsun@do.jiamingsun.ml "chown -R jiamingsun:www-data ~/homepage"
